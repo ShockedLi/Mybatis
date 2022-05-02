8a01c2b28899f9fe3d78851f227a5e5ba85fd28f
shockedli <979036525@qq.com>
2022-05-02 14:15:41 +0800

    resultMap commit

--- mybatis_proxy/src/main/java/com/shockedli/mybatis_bean/User.java
+++ mybatis_proxy/src/main/java/com/shockedli/mybatis_bean/User.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class User {
 
     private Integer id;
-    private String username;
-    private Date birthday;
-    private String sex;
-    private String address;
+    private String usernameabc;
+    private Date birthdayabc;
+    private String sexabc;
+    private String addressabc;
 
 
     public void setId(Integer id) {
@@ -16,29 +16,29 @@ public class User {
     }
 
     public void setUsername(String username) {
-        this.username = username;
+        this.usernameabc = username;
     }
 
     public void setBirthday(Date birthday) {
-        this.birthday = birthday;
+        this.birthdayabc = birthday;
     }
 
     public void setSex(String sex) {
-        this.sex = sex;
+        this.sexabc = sex;
     }
 
     public void setAddress(String address) {
-        this.address = address;
+        this.addressabc = address;
     }
 
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
-                ", username='" + username + '\'' +
-                ", birthday=" + birthday +
-                ", sex='" + sex + '\'' +
-                ", address='" + address + '\'' +
+                ", usernameabc='" + usernameabc + '\'' +
+                ", birthdayabc=" + birthdayabc +
+                ", sexabc='" + sexabc + '\'' +
+                ", addressabc='" + addressabc + '\'' +
                 '}';
     }
 }
